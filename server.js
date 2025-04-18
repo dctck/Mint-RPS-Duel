b628d912a30a9f56458f9683ca72d9ac8961057b
dctck <90426933+dctck@users.noreply.github.com>
2025-04-18 14:53:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const bodyParser = require("body-parser");
 const { request, gql } = require("graphql-request");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
